3b2995f1b1832c279c037f54d90d17d084dc4496
Athman Gude <athmangude@gmail.com>
2016-06-21 23:57:55 +0300

    cleaned out obsolete code, laying out items screen

--- app/components/MainSection.js
+++ app/components/MainSection.js
@@ -1,16 +1,16 @@
 import React, { Component, PropTypes } from 'react';
-import TodoItem from './TodoItem';
-import Footer from './Footer';
-import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../constants/TodoFilters';
-import style from './MainSection.css';
+import Avatar from 'material-ui/Avatar';
+import { List, ListItem } from 'material-ui/List';
+import RaisedButton from 'material-ui/RaisedButton';
+import Subheader from 'material-ui/Subheader';
+import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
+import {pinkA200, transparent} from 'material-ui/styles/colors';
 
-const TODO_FILTERS = {
-  [SHOW_ALL]: () => true,
-  [SHOW_ACTIVE]: todo => !todo.completed,
-  [SHOW_COMPLETED]: todo => todo.completed
+const style = {
+  margin: 12,
 };
 
-export default class MainSection extends Component {
+class MainSection extends Component {
 
   static propTypes = {
     todos: PropTypes.array.isRequired,
@@ -19,72 +19,92 @@ export default class MainSection extends Component {
 
   constructor(props, context) {
     super(props, context);
-    this.state = { filter: SHOW_ALL };
-  }
-
-  handleClearCompleted = () => {
-    const atLeastOneCompleted = this.props.todos.some(todo => todo.completed);
-    if (atLeastOneCompleted) {
-      this.props.actions.clearCompleted();
-    }
-  };
-
-  handleShow = filter => {
-    this.setState({ filter });
-  };
-
-  renderToggleAll(completedCount) {
-    const { todos, actions } = this.props;
-    if (todos.length > 0) {
-      return (
-        <input
-          className={style.toggleAll}
-          type="checkbox"
-          checked={completedCount === todos.length}
-          onChange={actions.completeAll}
-        />
-      );
-    }
-  }
-
-  renderFooter(completedCount) {
-    const { todos } = this.props;
-    const { filter } = this.state;
-    const activeCount = todos.length - completedCount;
-
-    if (todos.length) {
-      return (
-        <Footer
-          completedCount={completedCount}
-          activeCount={activeCount}
-          filter={filter}
-          onClearCompleted={this.handleClearCompleted}
-          onShow={this.handleShow}
-        />
-      );
-    }
   }
 
   render() {
-    const { todos, actions } = this.props;
-    const { filter } = this.state;
-
-    const filteredTodos = todos.filter(TODO_FILTERS[filter]);
-    const completedCount = todos.reduce(
-      (count, todo) => (todo.completed ? count + 1 : count),
-      0
-    );
-
     return (
-      <section className={style.main}>
-        {this.renderToggleAll(completedCount)}
-        <ul className={style.todoList}>
-          {filteredTodos.map(todo =>
-            <TodoItem key={todo.id} todo={todo} {...actions} />
-          )}
-        </ul>
-        {this.renderFooter(completedCount)}
+      <section>
+        <List>
+          <Subheader>Items in your cart</Subheader>
+          <ListItem
+            primaryText="Brendan Lim"
+            secondaryText={`5 x 500`}
+            leftAvatar={<Avatar src="images/ok-128.jpg" />}
+            rightAvatar={
+              <Avatar
+                color={pinkA200} backgroundColor={transparent}
+                style={{ right: 8, width: 60 }}
+              >
+                2,500
+              </Avatar>
+            }
+          />
+          <ListItem
+            primaryText="Eric Hoffman"
+            secondaryText={`1 x 3,000`}
+            leftAvatar={<Avatar src="images/kolage-128.jpg" />}
+            rightAvatar={
+              <Avatar
+                color={pinkA200} backgroundColor={transparent}
+                style={{ right: 8, width: 60 }}
+              >
+                3,000
+              </Avatar>
+            }
+          />
+          <ListItem
+            primaryText="Grace Ng"
+            secondaryText={`3 x 400`}
+            leftAvatar={<Avatar src="images/uxceo-128.jpg" />}
+            rightAvatar={
+              <Avatar
+                color={pinkA200} backgroundColor={transparent}
+                style={{ right: 8, width: 60 }}
+              >
+                1,200
+              </Avatar>
+            }
+          />
+          <ListItem
+            primaryText="Kerem Suer"
+            secondaryText={`10 x 4,000`}
+            leftAvatar={<Avatar src="images/kerem-128.jpg" />}
+            rightAvatar={
+              <Avatar
+                color={pinkA200} backgroundColor={transparent}
+                style={{ right: 8, width: 60 }}
+              >
+              40,000
+              </Avatar>
+            }
+          />
+          <ListItem
+            primaryText="Raquel Parrado"
+            secondaryText={`2 x 10,000`}
+            leftAvatar={<Avatar src="images/raquelromanp-128.jpg" />}
+            rightAvatar={
+              <Avatar
+                color={pinkA200} backgroundColor={transparent}
+                style={{ right: 8, width: 60 }}
+              >
+                20,000
+              </Avatar>
+            }
+          />
+        </List>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+          <RaisedButton
+          label="Checkout with Shopbuddy"
+          secondary={true}
+          style={style} />
+        </div>
       </section>
     );
   }
 }
+
+export default MainSection;
