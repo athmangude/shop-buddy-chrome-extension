06b558a23db34a0e0e8801a9fa49d3c856223fc7
Athman Gude <athmangude@gmail.com>
2016-06-21 17:22:48 +0300

    using muiThemeProvider and passing the app theme

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -2,6 +2,13 @@ import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import appTheme from '../appTheme.js';
+
+injectTapEventPlugin();
+
 export default class Root extends Component {
 
   static propTypes = {
@@ -11,9 +18,11 @@ export default class Root extends Component {
   render() {
     const { store } = this.props;
     return (
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </MuiThemeProvider>
     );
   }
 }
