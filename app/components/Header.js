896db2affe5bd9f40ac3758c1fa99823197bab12
Athman Gude <athmangude@gmail.com>
2016-06-21 23:14:43 +0300

    replaced header with appbar

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,8 +1,11 @@
 import React, { PropTypes, Component } from 'react';
 import TodoTextInput from './TodoTextInput';
 
-import { Card, CardActions, CardHeader, CardText } from 'material-ui';
-import FlatButton from 'material-ui/FlatButton';
+import AppBar from 'material-ui/AppBar';
+import FontIcon from 'material-ui/FontIcon';
+import IconButton from 'material-ui/IconButton';
+
+import CommunicationCall from 'material-ui/svg-icons/action/shopping-basket';
 
 export default class Header extends Component {
 
@@ -18,32 +21,10 @@ export default class Header extends Component {
 
   render() {
     return (
-      <header>
-        <h1>todos</h1>
-        <Card>
-          <CardHeader
-            title="Without Avatar"
-            subtitle="Subtitle"
-            actAsExpander={true}
-            showExpandableButton={true}
-          />
-          <CardText expandable={true}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions expandable={true}>
-            <FlatButton label="Action1" />
-            <FlatButton label="Action2" />
-          </CardActions>
-        </Card>
-        <TodoTextInput
-          newTodo
-          onSave={this.handleSave}
-          placeholder="What needs to be done?"
-        />
-      </header>
+      <AppBar
+        title="Shopbuddy"
+        iconElementLeft={<IconButton><CommunicationCall style={{ fontSize: 100 }} /></IconButton>}
+      />
     );
   }
 }
