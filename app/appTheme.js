a6aa6dbe06e2ae3ca3ef39b6362a0e98e63d7259
Athman Gude <athmangude@gmail.com>
2016-06-21 15:19:56 +0300

    added appTheme

---
+++ app/appTheme.js
@@ -0,0 +1,40 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = {
+  spacing: {
+    iconSize: 24,
+    desktopGutter: 24,
+    desktopGutterMore: 32,
+    desktopGutterLess: 16,
+    desktopGutterMini: 8,
+    desktopKeylineIncrement: 64,
+    desktopDropDownMenuItemHeight: 32,
+    desktopDropDownMenuFontSize: 15,
+    desktopDrawerMenuItemHeight: 48,
+    desktopSubheaderHeight: 48,
+    desktopToolbarHeight: 56,
+  },
+  fontFamily: 'Roboto, sans-serif',
+  palette: {
+    primary1Color: '#2196F3',
+    primary2Color: '#1976D2',
+    primary3Color: '#757575',
+    accent1Color: '#E64A19',
+    accent2Color: '#D84315',
+    accent3Color: '#F4511E',
+    textColor: '#212121',
+    alternateTextColor: 'white',
+    canvasColor: 'white',
+    borderColor: '#E0E0E0',
+    disabledColor: 'rgba(0, 0, 0, 0.3)',
+    pickerHeaderColor: '#2196F3',
+    clockCircleColor: 'rgba(0, 0, 0, 0.1)',
+    shadowColor: '#000',
+  }
+};
