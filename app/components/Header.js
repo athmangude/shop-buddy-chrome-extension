1346338ca58c2bbe772f79c211e97e67844d1a7d
Athman Gude <athmangude@gmail.com>
2016-06-21 17:20:33 +0300

    experimenting with card

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,9 @@
 import React, { PropTypes, Component } from 'react';
 import TodoTextInput from './TodoTextInput';
 
+import { Card, CardActions, CardHeader, CardText } from 'material-ui';
+import FlatButton from 'material-ui/FlatButton';
+
 export default class Header extends Component {
 
   static propTypes = {
@@ -17,6 +20,24 @@ export default class Header extends Component {
     return (
       <header>
         <h1>todos</h1>
+        <Card>
+          <CardHeader
+            title="Without Avatar"
+            subtitle="Subtitle"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+          <CardActions expandable={true}>
+            <FlatButton label="Action1" />
+            <FlatButton label="Action2" />
+          </CardActions>
+        </Card>
         <TodoTextInput
           newTodo
           onSave={this.handleSave}
