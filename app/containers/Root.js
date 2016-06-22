494ec6d2abc19582d0baf6c865771ed930cef667
Athman Gude <athmangude@gmail.com>
2016-06-22 12:42:16 +0300

    removed themeprovider and mui theme

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -2,10 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
 
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import appTheme from '../appTheme.js';
 
 injectTapEventPlugin();
 
@@ -18,11 +15,9 @@ export default class Root extends Component {
   render() {
     const { store } = this.props;
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </MuiThemeProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     );
   }
 }
