b4c48b495d391698cc926202341878759e5692f8
Athman Gude <athmangude@gmail.com>
2016-06-24 11:28:19 +0300

    requiring jquery, exported currency converter

--- chrome/extension/background/currencyConverter.js
+++ chrome/extension/background/currencyConverter.js
@@ -1,11 +1,14 @@
 // https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("USDKES")&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
-function currencyConverter(currency_from,currency_to,currency_input){
-    var yql_base_url = "https://query.yahooapis.com/v1/public/yql";
-    var yql_query = 'select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("'+currency_from+currency_to+'")';
-    var yql_query_url = yql_base_url + "?q=" + yql_query + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-    // var http_response = httpGet(yql_query_url);
-    // var http_response_json = JSON.parse(http_response);
-    //console.log(http_response);
+var $ = require('jquery');
 
-    // return http_response_json.query.results.rate.Rate;
+function currencyConverter(currencyFrom,currencyTo,currencyInput, callback){
+    var yqlBaseUrl = "https://query.yahooapis.com/v1/public/yql";
+    var yqlQuery = 'select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("'+currencyFrom+currencyTo+'")';
+    var yqlQueryUrl = yqlBaseUrl + "?q=" + yqlQuery + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+    $.get( yqlQueryUrl, function( response ) {
+        return callback(response.query.results.rate.Rate);
+    });
 }
+
+module.exports = currencyConverter;
