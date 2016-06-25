75410abe4d28e2565baccd63a34a8576397abd03
Athman Gude <athmangude@gmail.com>
2016-06-25 22:54:10 +0300

    moved totals and checkout button to footer module

--- app/components/shopping-cart/cart-items/index.js
+++ app/components/shopping-cart/cart-items/index.js
@@ -9,13 +9,8 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import accounting from 'accounting';
 
 import CartItem from './cart-item';
+import Footer from '../footer';
 import appTheme from '../../../appTheme.js';
-
-const primaryColor = getMuiTheme(appTheme).palette.primary1Color;
-
-const style = {
-    margin: 12,
-};
 
 class CartItems extends Component {
 
@@ -66,20 +61,7 @@ class CartItems extends Component {
                         <CartItem key={cartItem.id} cartItem={cartItem} />
                     ))}
                 </List>
-                <div style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    flexDirection: 'column'
-                }}>
-                    <Paper zDepth={0}>
-                        <h1 style={{ fontWeight: 'normal', color: primaryColor }}>{`${accounting.formatMoney(this.state.total, { symbol: 'KES', format: '%s %v' })}`}/-</h1>
-                    </Paper>
-                    <RaisedButton
-                        label="Checkout with Shopbuddy"
-                        secondary={true}
-                        style={style} />
-                </div>
+                <Footer total={this.state.total} />
             </section>
         );
     }
