d48f82d879a60a0b76cd9e397e7dc63ece9002bd
Athman Gude <athmangude@gmail.com>
2016-06-26 15:11:28 +0300

    opening the dialog when cart item is touch tapped

--- app/components/shopping-cart/cart-items/cart-item/index.js
+++ app/components/shopping-cart/cart-items/cart-item/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { ListItem } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 import accounting from 'accounting';
@@ -9,14 +9,25 @@ import { transparent } from 'material-ui/styles/colors';
 const primaryColor = getMuiTheme(appTheme).palette.primary1Color;
 
 class CartItem extends Component {
+
+    static propTypes = {
+        cartItem: PropTypes.object.isRequired,
+        onOpenCartItemDialog: PropTypes.func.isRequired,
+    };
+
     constructor(props) {
         super(props);
+    }
+
+    onCartItemTouchTap() {
+        this.props.onOpenCartItemDialog(this.props.cartItem);
     }
 
     render() {
         const cartItem = this.props.cartItem;
         return (
             <ListItem
+                onTouchTap={this.onCartItemTouchTap.bind(this)}
                 primaryText={<div style={{ width: '87%' }}>{this.props.cartItem.title}</div>}
                 secondaryText={`${cartItem.quantity} x ${accounting.formatMoney(cartItem.price, {symbol: '', format: "%s %v" })}`}
                 leftAvatar={<Avatar src={cartItem.imageUrl} />}
