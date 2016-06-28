5bad4f1fb8344eef1721cf7edd8244952788fc98
Athman Gude <athmangude@gmail.com>
2016-06-28 10:17:00 +0300

    added button illustration to empty cart component

--- app/components/shopping-cart/cart-items/empty-cart/index.js
+++ app/components/shopping-cart/cart-items/empty-cart/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Card, CardText, CardTitle, Paper } from 'material-ui';
+import { Card, CardText, CardTitle, Paper, RaisedButton } from 'material-ui';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import appTheme from '../../../../appTheme.js';
+const accent1Color = getMuiTheme(appTheme).palette.accent1Color;
 
 class EmptyCart extends Component {
     constructor(props) {
@@ -11,7 +14,7 @@ class EmptyCart extends Component {
             <Paper zDepth={0}>
                 <CardTitle title="There are no items in your cart" />
                 <CardText>
-                    To add items into your cart, add them to your Amazon cart as you would while shopping normally on Amazon. Then navigate to your cart to view your cart items. Then click on the <strong>SHOPBUDDY CHECKOUT</strong> button to proceed to checkout.
+                    To add items into your cart, add them to your Amazon cart as you would while shopping normally on Amazon. Then navigate to your cart to view your cart items. Then click on the <RaisedButton secondary={true} disabled={true} style={{ fontWeight: 'bold', color: 'white', display: 'inline-block', paddingLeft: 10, paddingRight: 10, backgroundColor: accent1Color }}>SHOPBUDDY CHECKOUT</RaisedButton> button to reveal your shopbuddy cart and proceed to checkout.
                 </CardText>
             </Paper>
         );
