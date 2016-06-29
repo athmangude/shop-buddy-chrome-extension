12c2def9360b1cdb1f9d72702f7ab1bc60653057
Athman Gude <athmangude@gmail.com>
2016-06-29 10:45:59 +0300

    added payment options component

---
+++ app/components/shopping-cart/footer/checkout/payment-options/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class PaymentMethods extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Choose a payment Method</h3>
+            </div>
+        );
+    }
+}
+
+export default PaymentMethods;
