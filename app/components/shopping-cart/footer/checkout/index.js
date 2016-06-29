26c0825ccab2f03d201315c53aa548280c584a99
Athman Gude <athmangude@gmail.com>
2016-06-29 10:44:06 +0300

    progress with the checkout process

--- app/components/shopping-cart/footer/checkout/index.js
+++ app/components/shopping-cart/footer/checkout/index.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
-import { Dialog, CircularProgress } from 'material-ui';
+import { Dialog, CircularProgress, FlatButton } from 'material-ui';
+
+import PaymentOptions from './payment-options';
 
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import appTheme from '../../../../appTheme.js';
 const accent1Color = getMuiTheme(appTheme).palette.accent1Color;
+
+
 
 class Checkout extends Component {
     constructor(props) {
         super(props);
     }
 
+    onCheckoutCancelled() {
+        this.props.appActions.cancelCheckout();
+    }
+
     render() {
+        console.log(this.props.app.isCheckingOut);
+        let dialogComponent;
+        this.props.app.isSendingCart ? dialogComponent = <CircularProgress size={2} secondary={true} color={accent1Color} /> : dialogComponent = <PaymentOptions { ...this.props } />
+
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                secondary={true}
+                disabled={!this.props.app.isCartSendingResponseReceived}
+                onTouchTap={this.onCheckoutCancelled.bind(this)}
+            />,
+        ];
+
         return (
             <Dialog
-                title="Hang tight! We are Sending your cart to our Servers"
+                title={this.props.app.isSendingCart ? "Hang tight! We are Sending your cart to our Servers" : "We Have Received your Cart"}
                 titleStyle={{textAlign: 'center'}}
                 modal={true}
-                open={this.props.isCheckingOut}
+                actions={actions}
+                open={this.props.app.isCheckingOut}
             >
                 <div style={{
                     display: 'flex',
                     // alignItems: 'center',
                     justifyContent: 'center'
                 }}>
-                    <CircularProgress size={2} secondary={true} color={accent1Color} />
+                    {dialogComponent}
                 </div>
             </Dialog>
         )
