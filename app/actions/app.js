925196230955d40ebd461e2205e56a18d35509f6
Athman Gude <athmangude@gmail.com>
2016-06-29 10:42:18 +0300

    added reducers, actions and constants for checkout process

--- app/actions/app.js
+++ app/actions/app.js
@@ -4,6 +4,18 @@ export function beginCheckout() {
   return { type: types.BEGIN_CHECKOUT };
 }
 
+export function cancelCheckout() {
+  return { type: types.CANCEL_CHECKOUT };
+}
+
 export function endCheckout() {
   return { type: types.END_CHECKOUT };
+}
+
+export function sendCart() {
+  return { type: types.SEND_CART };
+}
+
+export function receiveCartSendingResponse() {
+  return { type: types.RECEIVE_CART_SENDING_RESPONSE };
 }
--- app/reducers/app.js
+++ app/reducers/app.js
@@ -2,18 +2,29 @@ import * as AppActionTypes from '../constants/appActionTypes';
 
 const initialState = {
     isCheckingOut: false,
+    isSendingCart: false,
+    isCartSendingResponseReceived: false,
 };
 
 const actionsMap = {
   [AppActionTypes.BEGIN_CHECKOUT](state, action) {
-    return Object.assign({}, state, { isCheckingOut: true });
+    return Object.assign({}, state, { isCheckingOut: true, });
+  },
+  [AppActionTypes.SEND_CART](state, action) {
+    return Object.assign({}, state, { isSendingCart: true, isCartSendingResponseReceived: false, });
+  },
+  [AppActionTypes.RECEIVE_CART_SENDING_RESPONSE](state, action) {
+    return Object.assign({}, state, { isSendingCart: false, isCartSendingResponseReceived: true, });
+  },
+  [AppActionTypes.CANCEL_CHECKOUT](state, action) {
+    return Object.assign({}, state, { isCheckingOut: false, isSendingCart: false, isCartSendingResponseReceived: false, })
   },
   [AppActionTypes.END_CHECKOUT](state, action) {
-    return Object.assign({}, state, { isCheckingOut: false })
+    return Object.assign({}, state, { isCheckingOut: false, })
   },
 };
 
-export default function cartItems(state = initialState, action) {
+export default function app(state = initialState, action) {
   const reduceFn = actionsMap[action.type];
   if (!reduceFn) return state;
   return reduceFn(state, action);
