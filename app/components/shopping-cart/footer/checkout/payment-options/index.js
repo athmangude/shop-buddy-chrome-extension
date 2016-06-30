ebe9e89da3227941cf39e14e6f15301c47d9ec6d
Athman Gude <athmangude@gmail.com>
2016-06-30 18:16:10 +0300

    added make payment method

--- app/components/shopping-cart/footer/checkout/payment-options/index.js
+++ app/components/shopping-cart/footer/checkout/payment-options/index.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { RaisedButton } from 'material-ui';
 
 class PaymentMethods extends Component {
     constructor(props) {
         super(props);
     }
 
+    onMakePaymentButtonTouchTap() {
+        let win = window.open('http://shopbuddy.online/', '_blank');
+        win.focus();
+
+        this.props.appActions.endCheckout();
+        this.props.cartActions.emptyCart();
+    }
+
     render() {
         return (
             <div>
-                <h3>Choose a payment Method</h3>
+                <RaisedButton
+                    label="Make your payment"
+                    primary={true}
+                    onTouchTap={this.onMakePaymentButtonTouchTap.bind(this)}
+                />
             </div>
         );
     }
