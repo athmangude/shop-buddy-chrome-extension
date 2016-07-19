cdd3f14ecbde850745af96be99c8a82236c29ace
Athman Gude <athmangude@gmail.com>
2016-07-19 06:03:47 +0300

    combining authentication reducer into the main reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import cartItems from './cartItems';
 import app from './app';
+import authentication from './authentication';
 
 export default combineReducers({
   cartItems,
   app,
+  authentication,
 });
