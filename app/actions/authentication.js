be7ced4dc1b391fefcff464a314a05eab0343ccc
Athman Gude <athmangude@gmail.com>
2016-07-20 10:58:26 +0300

    added actions to set user auths and profile, passing the user to reducer when ending signining in

--- app/actions/authentication.js
+++ app/actions/authentication.js
@@ -4,10 +4,32 @@ export function beginSigningIn() {
   return { type: types.BEGIN_SIGNING_IN };
 }
 
-export function endSigningIn() {
-  return { type: types.END_SIGNING_IN };
+export function endSigningIn(user) {
+  return { type: types.END_SIGNING_IN, user };
 }
 
 export function signOut() {
   return { type: types.SIGN_OUT };
+}
+
+
+export function setAuthToken(authToken) {
+  return {
+    type: types.SET_AUTH_TOKEN,
+    authToken,
+  }
+}
+
+export function setChromeUser(chromeUser) {
+  return {
+    type: types.SET_CHROME_USER,
+    chromeUser,
+  }
+}
+
+export function setGplusProfile(gplusProfile) {
+  return {
+    type: types.SET_GPLUS_PROFILE,
+    gplusProfile,
+  }
 }
