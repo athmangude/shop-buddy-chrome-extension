caf84f622ad6be73d5b7ed2e2b27542f8555d70b
Athman Gude <athmangude@gmail.com>
2016-11-08 06:53:39 +0300

    house keeping changes

--- app/components/window-app/index.js
+++ app/components/window-app/index.js
@@ -4,21 +4,21 @@ import AccountAndHistory from './account-and-history';
 import SignIn from './sign-in';
 
 class WindowApp extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    if (!this.props.authentication.isSignedIn) {
+      return (
+        <SignIn { ...this.props } />
+      );
     }
 
-    render() {
-        if (!this.props.authentication.isSignedIn) {
-            return (
-                <SignIn { ...this.props } />
-            )
-        }
-
-        return (
-            <AccountAndHistory { ...this.props } />
-        )
-    }
+    return (
+      <AccountAndHistory { ...this.props } />
+    );
+  }
 }
 
 export default WindowApp;
