822e6acf063d8d3e1303391e56d364a934cd5e08
Athman Gude <athmangude@gmail.com>
2016-11-13 03:04:19 +0300

    implemented pay with Mpesa

--- app/components/shopping-cart/footer/checkout/payment-options/index.js
+++ app/components/shopping-cart/footer/checkout/payment-options/index.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 import { RaisedButton } from 'material-ui';
+import { FormsyText } from 'formsy-material-ui/lib';
+import Formsy, { Form } from 'formsy-react';
+import numeral from 'numeral';
 
-class PaymentMethods extends Component {
+import MpesaIcon from 'material-ui/svg-icons/hardware/smartphone';
+import CreditCardIcon from 'material-ui/svg-icons/action/credit-card';
+
+const styles = {
+    buttons: {
+        margin: 5,
+    }
+}
+
+class PaymentOptions extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            selectedPaymentMethod: 'mpesa',
+            canSubmit: false,
+        }
+
+        this.onMpesaConfirmationCodeChanged = this.onMpesaConfirmationCodeChanged.bind(this);
+        // this.enableButton = this.enableButton.bind(this);
+        // this.disableButton = this.disableButton.bind(this);
     }
 
     onMakePaymentButtonTouchTap() {
@@ -14,17 +35,90 @@ class PaymentMethods extends Component {
         this.props.cartActions.emptyCart();
     }
 
+    onMpesaConfirmationCodeChanged(event) {
+        this.setState({
+            mpesaConfirmationCode: event.target.value,
+        });
+    }
+
+    disableButton() {
+        this.props.submitChanged(false);
+    }
+
+    enableButton() {
+        this.props.submitChanged(true);
+    }
+
+    notifyFormError(data) { // eslint-disable-line no-unused-vars
+        // console.error('Form error:', data);
+    }
+
+    submitForm(data) {
+        console.log(data);
+    }
+
+    renderPaymentBox() {
+        if (this.state.selectedPaymentMethod === 'mpesa') {
+            return (
+                <div>
+                    <p>Pay <strong>KES {numeral(Math.ceil(this.props.total)).format('0,0.00')}</strong> to Mpesa Business #123456, Account #{this.props.authentication.signedInUser.gplusProfile.id}</p>
+                    <Form
+                        onValid={this.enableButton.bind(this)}
+                        onInvalid={this.disableButton.bind(this)}
+                        onValidSubmit={this.submitForm}
+                        onInvalidSubmit={this.notifyFormError}
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <FormsyText
+                            name="confirmationCode"
+                            type="text"
+                            value={this.state.mpesaConfirmationCode}
+                            validations={{ isAlphanumeric: true, minLength: 10, maxLength: 10, }}
+                            validationErrors={{
+                                isAlphanumeric: 'invalid code',
+                                minLength: 'too short',
+                                maxLength: 'too long',
+                            }}
+                            required
+                            hintText="Mpesa confrimation code"
+                            floatingLabelText="Mpesa confirmation code"
+                            onChange={this.onMpesaConfirmationCodeChanged}
+                            inputStyle={{ textTransform: 'uppercase' }}
+                        />
+                    </Form>
+                </div>
+            );
+        }
+    }
+
     render() {
         return (
             <div>
                 <RaisedButton
-                    label="Make your payment"
-                    primary={true}
-                    onTouchTap={this.onMakePaymentButtonTouchTap.bind(this)}
+                    label="Mpesa"
+                    backgroundColor="#8bc43f"
+                    hoverColor="#97c956"
+                    icon={<MpesaIcon />}
+                    // onTouchTap={this.onMakePaymentButtonTouchTap.bind(this)}
+                    style={Object.assign({}, styles.buttons)}
                 />
+                <RaisedButton
+                    label="Card"
+                    // primary={true}
+                    icon={<CreditCardIcon />}
+                    disabled
+                    // onTouchTap={this.onMakePaymentButtonTouchTap.bind(this)}
+                    style={styles.buttons}
+                />
+                {this.renderPaymentBox()}
             </div>
         );
     }
 }
 
-export default PaymentMethods;
+export default PaymentOptions;
