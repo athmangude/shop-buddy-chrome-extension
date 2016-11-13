b052597b227de62fd76f89d254d6e043f14f680f
Athman Gude <athmangude@gmail.com>
2016-11-13 03:03:09 +0300

    updating cart payment info on firebase

--- app/components/shopping-cart/footer/checkout/index.js
+++ app/components/shopping-cart/footer/checkout/index.js
@@ -12,28 +12,82 @@ const accent1Color = getMuiTheme(appTheme).palette.accent1Color;
 class Checkout extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            canSubmit: false,
+        }
+
+        this.onCheckoutConfirmed = this.onCheckoutConfirmed.bind(this);
+        this.onCheckoutCancelled = this.onCheckoutCancelled.bind(this);
+        this.onSubmitChanged = this.onSubmitChanged.bind(this);
     }
 
     onCheckoutCancelled() {
         this.props.appActions.cancelCheckout();
     }
 
+    onCheckoutConfirmed() {
+        const mpesaConfirmationCode = this.refs.paymentOptions.state.mpesaConfirmationCode;
+        const cartId = this.props.cartId;
+
+        this.props.firebaseRefs.database.carts.child(cartId).update({
+            paymentInfo: {
+                paid: true,
+                paymentType: 'mpesa',
+                mpesaConfirmationCode: mpesaConfirmationCode,
+                paymentConfirmed: false,
+            }
+        }, (error) => {
+            if (error) {
+                return console.log('there was an error updating the cart');
+            }
+            console.log('payment details were updated successfully');
+            this.props.appActions.endCheckout();
+            this.props.cartActions.emptyCart();
+        });
+    }
+
+    onSubmitChanged(status) {
+        this.setState({
+            canSubmit: status,
+        });
+    }
+
+
+    getDialogTitle() {
+      if (this.props.app.isSendingCart) {
+        return 'Hang tight! We are Sending your cart to our Servers'
+      }
+
+      if (!this.props.app.checkoutError) {
+        return 'Select Your Payment Method'
+      }
+
+      return 'There was an error sending your cart'
+    }
+
     render() {
         let dialogComponent;
-        this.props.app.isSendingCart ? dialogComponent = <CircularProgress size={2} secondary={true} color={accent1Color} /> : dialogComponent = <PaymentOptions { ...this.props } />
+        this.props.app.isSendingCart ? dialogComponent = <CircularProgress size={2} secondary={true} color={accent1Color} /> : dialogComponent = <PaymentOptions ref="paymentOptions" { ...this.props } submitChanged={this.onSubmitChanged} />
 
         const actions = [
             <FlatButton
                 label="Cancel"
-                secondary={true}
+                secondary
                 disabled={!this.props.app.isCartSendingResponseReceived}
-                onTouchTap={this.onCheckoutCancelled.bind(this)}
+                onTouchTap={this.onCheckoutCancelled}
+            />,
+            <FlatButton
+                label="Submit"
+                primary
+                disabled={!(this.props.app.isCartSendingResponseReceived && this.state.canSubmit)}
+                onTouchTap={this.onCheckoutConfirmed}
             />,
         ];
 
         return (
             <Dialog
-                title={this.props.app.isSendingCart ? "Hang tight! We are Sending your cart to our Servers" : "We Have Received your Cart"}
+                title={this.getDialogTitle()}
                 titleStyle={{textAlign: 'center'}}
                 modal={true}
                 actions={actions}
