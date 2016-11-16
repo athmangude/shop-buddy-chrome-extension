d54f219b5c5aa5a6819f1bc3d57c04973860b1f1
Athman Gude <athmangude@gmail.com>
2016-11-16 11:08:37 +0300

    showing no order history when there are no orders, getting started rendering orders

--- app/components/window-app/account-and-history/transaction-history/index.js
+++ app/components/window-app/account-and-history/transaction-history/index.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
+import ShoppingBasket from 'material-ui/svg-icons/action/shopping-basket';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import appTheme from '../../../../appTheme.js';
+const primaryColor = getMuiTheme(appTheme).palette.primary1Color;
+const accent1Color = getMuiTheme(appTheme).palette.accent1Color;
 
 import Transaction from './transaction';
 
+const styles = {
+  centeringFlexContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    paddingTop: 70,
+  },
+  link: {
+    color: accent1Color,
+    textDecoration: 'none',
+    fontWeight: 'bold',
+    ':hover': {
+      textDecoration: 'underline',
+    }
+  }
+}
+
 class TransactionHistory extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+
+  onLinkClicked(event) {
+    event.preventDefault();
+    let win = window.open('https://www.amazon.com/', '_blank');
+    win.focus();
+  }
+
+  renderTransaction() {
+    if(!this.props.transactions.length) {
+      return (
+        <div
+          style={Object.assign({}, styles.centeringFlexContainer, {})}
+        >
+          <ShoppingBasket style={{ height: 100, width: 100, color: '#232f3e', margin: 0, }} />
+          <h4>You have not submitted any carts</h4>
+          <p>Visit <a href="https://www.amazon.com/" style={styles.link} onClick={this.onLinkClicked}>Amazon.com</a> and checkout with Shopbuddy to submit your cart</p>
+        </div>
+      )
     }
 
-    render() {
-        return (
-            <div>
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
-            </div>
-        );
-    }
+    return this.props.transactions.map(transaction => (
+      <Transaction />
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderTransaction()}
+      </div>
+    );
+  }
 }
 
 export default TransactionHistory;
